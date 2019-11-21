5966cfd66809360fef16683381371ebebbd20d1b
yangzw <yangzw@frtzfund.com>
2019-11-21 13:34:23 +0800

    standalone util to cache pypi to local file

--- src/pip/_internal/gui/myglobals.py
+++ src/pip/_internal/gui/myglobals.py
@@ -1,7 +1,13 @@
 # my globals here
 
+PYPI_INDEX_RAW = "pypi_index_raw.txt"
+PYPI_INDEX_PY = "pypi_index.py"
+PYPI_INDEX_DAT = "pypi_index.dat"
+
+
 class MyGlobals:
     pass
 
 
 g = MyGlobals()
+
---
+++ src/pip/_internal/gui/pypi_index_update.py
@@ -0,0 +1,29 @@
+
+from myglobals import *
+
+import urllib.request
+import re
+import pprint
+
+
+def step1_pypi_index_raw():
+    url = "https://pypi.org/simple"
+    with urllib.request.urlopen(url) as f:
+        open(PYPI_INDEX_RAW, "wb").write(f.read())
+
+
+def step2_pypi_index_py():
+    d = dict()
+    re_href = re.compile('<a href="(?P<href>.*?)">(?P<name>.*?)</a>')
+    for lineb in open(PYPI_INDEX_RAW, "rb"):
+        line = lineb.decode("utf-8")
+        m = re_href.search(line)
+        if m is None: continue
+        print(m.group("href"), m.group("name"))
+        d[m.group("name")] = m.group("href")
+    open(PYPI_INDEX_PY, "w").write("pypi_index = " + pprint.pformat(d))
+
+
+if __name__ == '__main__':
+    # step1_pypi_index_raw()
+    step2_pypi_index_py()
