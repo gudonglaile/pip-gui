45a437b6d105546b2540f8f127f6d93b7742e143
yangzw <yangzw@frtzfund.com>
2019-11-21 18:00:08 +0800

    dat file instead of py file python takes too much pain to import big .py

--- src/pip/_internal/gui/pypi_index_update.py
+++ src/pip/_internal/gui/pypi_index_update.py
@@ -24,6 +24,19 @@ def step2_pypi_index_py():
     open(PYPI_INDEX_PY, "w").write("pypi_index = " + pprint.pformat(d))
 
 
+def step3_pypi_index_dat():
+    f = open(PYPI_INDEX_DAT, "w")
+    re_href = re.compile('<a href="(?P<href>.*?)">(?P<name>.*?)</a>')
+    for lineb in open(PYPI_INDEX_RAW, "rb"):
+        line = lineb.decode("utf-8")
+        m = re_href.search(line)
+        if m is None: continue
+        name = m.group("name")
+        assert " " not in name
+        print(name, m.group("href"), file=f)
+
+
 if __name__ == '__main__':
     # step1_pypi_index_raw()
-    step2_pypi_index_py()
+    # step2_pypi_index_py()
+    step3_pypi_index_dat()
